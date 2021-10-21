adde41cd2d3527787e4d4e85d1ccf2910160f373
Ibarra <aitoribarra@tutanota.com>
2021-10-21 13:00:55 +0200

    Add home page

---
+++ portfolio-site-app/src/pages/index.styled.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  padding-top: 40vh;
+  .home {
+    letter-spacing: 0.3rem;
+    height: 100%;
+    width: 100%;
+    text-align: center;
+    font-size: 2rem;
+    align-items: center;
+    color: white;
+    &__description {
+      padding-top: 5px;
+    }
+
+    &__name {
+      display: flex;
+      justify-content: center;
+      font-size: 3rem;
+      padding: 10px;
+      border-bottom: 2px solid #fff;
+      &-first {
+        color: #61dafb;
+        margin-right: 10px;
+      }
+    }
+  }
+`;
--- portfolio-site-app/src/pages/index.tsx
+++ portfolio-site-app/src/pages/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Wrapper } from "./index.styled";
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <Wrapper>
+      <div className="home">
+        <div className="home__name">
+          <div className="home__name-first">Aitor</div>
+          <div className="home__name-last">Ibarra</div>
+        </div>
+        <div className="home__description">
+          Chemist, Developer & Photographer
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Home;
