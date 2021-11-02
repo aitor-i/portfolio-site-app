c36053ef71bcff977537a1cf1d92ce5820c65d95
Ibarra <aitoribarra@tutanota.com>
2021-11-02 12:46:20 +0100

    feat: add theme

---
+++ portfolio-site-app/src/theme.ts
@@ -0,0 +1,14 @@
+export interface ThemeType {
+  primaryColor: string;
+  secondaryColor: string;
+  lightSecondaryColor: string;
+  white: string;
+  black: string;
+}
+export const theme: ThemeType = {
+  primaryColor: "#61dafb",
+  secondaryColor: " #282c34",
+  lightSecondaryColor: "#3e4249",
+  white: "#f2f2f2",
+  black: " #212121",
+};
